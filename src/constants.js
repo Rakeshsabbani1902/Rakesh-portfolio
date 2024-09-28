8a94fa86644e0f5bd034405dcf690d627fee331d
Rakesh <rakeshsabbani1902@gmail.com>
2024-09-28 15:04:04 +0530

    Final commit

---
+++ src/constants.js
@@ -0,0 +1,37 @@
+
+export const services = [
+    {
+        title: "React Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Frontend Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    
+];
+
+export const name = 'Rakesh';
+
+export const experiences = [
+    {
+        'company': 'Infosys Ltd',
+        'role': 'Frontend Developer',
+        'duration': 'August 2022 - Present',
+        'logo': 'https://w7.pngwing.com/pngs/880/528/png-transparent-infosys-logo-and-slogan-thumbnail.png',
+        'points': [
+            'Involved in dynamic contributions to web development projects, actively influencing and improving project outcomes.',
+            'Successfully built and enhanced various web pages and interfaces, focusing on user engagement and performance optimization.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': '',
+    },
+    
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
